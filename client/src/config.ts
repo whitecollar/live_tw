5ecd1089e6d7ef10a1c0bc2d1ecc01e8bf5e14a1
antipin.a <antipin.a@local.rk-env.ru>
2024-01-27 18:13:03 +0300

    change adress backend

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
 // export const SERVER_ADDRESS = 'http://192.168.0.3:4001'
-export const SERVER_ADDRESS = 'https://заманье24.рф/live'
+export const SERVER_ADDRESS = 'https://livetw.timingweb.com/live'
